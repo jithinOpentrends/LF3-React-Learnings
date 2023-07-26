5b71909ff0e2d7b1b3a698ec2bd789d9ce554682
AthulMarutheri <athuls@opentrends.net>
2023-07-26 15:36:29 +0530

    conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,62 @@
 import "./App.css";
 import React from "react";
-import Main from "./componets/haripriya/Main";
+// import Main from "./componets/haripriya/Main";
+import Apps from "./componets/athul/Apps";
+
+//Conditional rendering IF and ELSE
+// function App() {
+
+//   const CheckNumber = ({number}) => {
+//     if (number > 0) {
+//       return <h1>positive number</h1>
+//     }
+//     else if (number < 0) {
+//       return <h2>negATIVE</h2>
+
+//     }
+//     else {
+//       return <h1> zero</h1>
+//     }
+//   };
+//   return (
+//     <div>
+//        <CheckNumber number={1}/>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+//Conditional rendering JAVASCRIPT EXPRESSION AYIT
+
+// function App() {
+//   let number = -1;
+//   return (
+//     <div>
+//       {number > 0 ? (
+//         <h1>positive</h1>
+//       ) : number < 0 ? (
+//         <h2>negATIVE</h2>
+//       ) : (
+//         <h1>zero</h1>
+//       )}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+  
+
+// //Conditional rendering JAVASCRIPT EXPRESSION IN COMPONENT
+// import M from "./Components/contitional/Main";
 
 function App() {
+ 
   return (
-    <>
-      <Main />
-    </>
+    <div>
+      <Apps isLoggedin={ true} />
+    </div>
   );
 }
 
---
+++ src/componets/athul/Apps.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Main from "./conditional/Main";
+
+
+function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+export default App;
